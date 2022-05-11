13ec6e3be3b041dfb7bd6b5ab6ea484ed9695d31
regis-oliveira <regis_crysis@hotmail.com>
2022-05-11 14:03:41 -0300

    fix: layout email and payload error

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -37,11 +37,29 @@ export class SubmitFeedbackUseCase {
     await this.mailAdapter.sendMail({
       subject: 'Novo feedback',
       body: [      
-        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-          `<p>Tipo do feedback: ${type}</p>`,
-          `<p>Comentário: ${comment}</p>`,
-          screenshot ? `<img src="${screenshot}" alt="It's a screenshot from feedback of type ${type}" />` : null,
-        `</div>`
+        `<div style="flex: 1; align-items: center; justify-content: center; background-color: #09090A; height: auto;">`,
+          `<div>`,
+            `<h1 style="font-family: sans-serif; text-align: center; color: #8257E5; padding-top: 20px">Novo feedback recebido</h1>`,        
+          `</div>`,
+          `<div style="background-color: #18181B; margin: 20px; padding: 20px">`,
+            `<div style="display: flex; align-items: center; justify-content: flex-start; flex-direction: row; margin-bottom: 14px;">`,
+              `<h3 style="color: #8257E5; font-family: sans-serif; font-weight: bold; margin-right: 6px">Tipo do feedback:</h3>`,
+              `<p style="font-family: sans-serif; color: #fff; font-size: 16px; font-weight: 400; align-self: center">${type}</p>`,
+            `</div>`,
+            `<div style="display: flex; align-items: center; justify-content: flex-start; flex-direction: row;">`,
+              `<h3 style="color: #8257E5; font-family: sans-serif; font-weight: bold; margin-right: 6px">Comentário:</h3>`,
+              `<p style="font-family: sans-serif; color: #fff; font-size: 16px; font-weight: 400; align-self: center">${comment}</p>`,        
+            `</div>`,
+          `</div>`,
+          `<div style="background-color: #18181B; margin: 0 20px; padding: 20px; display: flex; align-items: center; justify-content: flex-start; flex-direction: row;">`,
+            `<h3 style="color: #8257E5; font-family: sans-serif; font-weight: bold;">Print da tela abaixo:</h3>`,
+          `</div>`,
+          `<div style="background-color: #18181B; margin: 0 20px 0; padding: 20px; display: flex; align-items: center; justify-content: center; flex-direction: row; padding-bottom: 20px">`,
+            `<div style="border: 1px solid blueviolet">`,
+              screenshot ? `<img style="width: 900px; height: 900px;" src="${screenshot}" alt="It's a screenshot from feedback of type ${type}" />` : null,
+            `</div>`,
+          `</div>`,
+        `</div>`,        
       ].join('\n')      
     })
   }
